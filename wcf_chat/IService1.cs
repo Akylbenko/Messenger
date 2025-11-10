dc87dcefd960f0e6305f3213764d2ba36494b943
Akylbenko <licenzeeya@gmail.com>
2025-11-10 21:28:53 +1000

    Добавьте файлы проекта.

---
+++ wcf_chat/IService1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcf_chat
+{
+    // ПРИМЕЧАНИЕ. Можно использовать команду "Переименовать" в меню "Рефакторинг", чтобы изменить имя интерфейса "IService1" в коде и файле конфигурации.
+    [ServiceContract(CallbackContract = typeof(Iservice1Callback))]
+    public interface IService1
+    {
+        [OperationContract]
+        int Connect(string name);
+
+        [OperationContract]
+        void Disonnect(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void SendMessage(string message, int id);
+    }
+
+    public interface Iservice1Callback
+    {
+        [OperationContract(IsOneWay = true)]
+        void MessageCallback(string message);
+    }
+}
---
+++ wcf_chat/Service1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Security.Tokens;
+using System.Text;
+
+namespace wcf_chat
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]   
+    
+    public class Service1 : IService1
+    {   
+        List<ServerUser> users = new List<ServerUser>();
+        int nextId = 1;
+        public int Connect(string name)
+        {
+            ServerUser user = new ServerUser()
+            {
+                Id = nextId,
+                Name = name,
+                OperationContext = OperationContext.Current
+            };
+            nextId++;
+            SendMessage(user.Name + "подключился к чату!", 0);
+            users.Add(user);
+            return user.Id;
+        }
+
+        public void Disonnect(int id)
+        {
+            var user = users.FirstOrDefault(i=i => i.Id == id);
+            if (user != null)
+            {
+                users.Remove(user);
+                SendMessage(user.Name + "покинул чат!", 0);
+            }
+        }
+
+        public void SendMessage(string message, int id)
+        {
+            foreach (var item in users)
+            {
+                string answer = DateTime.Now.ToShortTimeString();
+                var user = users.FirstOrDefault(i = i => i.Id == id);
+                if (user != null)
+                {
+                    answer += ": " + user.Name + "";
+                }
+
+                answer += message;
+
+                item.OperationContext.GetCallbackChannel<Iservice1Callback>().MessageCallback(answer);
+            }
+        }
+    }
+}
